8883755da2e8a3a9d0684a3ce4d31141200a66fa
Patrick Chow <PatrickChow0803@gmail.com>
2019-11-04 15:42:34 -0500

    Patrick Chow: Initial Commit- Country Assignment

---
+++ assignment/src/main/java/local/patrickchow/country/countryassignment/CountryAssignmentApplication.java
@@ -0,0 +1,13 @@
+package local.patrickchow.country.countryassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountryAssignmentApplication.class, args);
+    }
+
+}
